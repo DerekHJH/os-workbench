d6c0083372594555f78a9948190091cd95db80c2
tracer-nju <tracer@nju.edu.cn>
2020-05-30 14:39:46 +0800

    Static hostname: Derekdebaby          Icon name: computer-laptop            Chassis: laptop         Machine ID: a95814c1cd184d5fbc55aa9ce988eab7            Boot ID: b3d4eaccc3324656bfebbfc118c85530   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-53-generic       Architecture: x86-64  14:39:46 up 7 min,  1 user,  load average: 0.60, 0.86, 0.55

---
+++ libkvdb/test/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+void test_1()
+{
+
+}
+int main() 
+{
+    setbuf(stdout, NULL);
+
+    printf("Test #1\n");
+    test_1();
+
+    return 0;
+}
