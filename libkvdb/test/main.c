2c806435265d341660ae45ff9a5b1743ed03d27b
tracer-nju <tracer@nju.edu.cn>
2020-05-30 14:57:43 +0800

    Static hostname: Derekdebaby          Icon name: computer-laptop            Chassis: laptop         Machine ID: a95814c1cd184d5fbc55aa9ce988eab7            Boot ID: b3d4eaccc3324656bfebbfc118c85530   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-53-generic       Architecture: x86-64  14:57:43 up 25 min,  1 user,  load average: 0.42, 0.41, 0.44

--- libkvdb/test/main.c
+++ libkvdb/test/main.c
@@ -3,6 +3,17 @@
 #include <assert.h>
 #include <string.h>
 #include <kvdb.h>
+
+#define panic_on(cond, s)\
+	do\
+	{\
+		if(cond)\
+		{\
+			printf(s);\
+			assert(0);\
+		}\
+	}while(0)
+
 int main() 
 {
   struct kvdb *db;
