e1c849220a0a267ff29f019b812f3cc3704ec37d
tracer-nju <tracer@nju.edu.cn>
2020-06-01 20:41:34 +0800

    Static hostname: Derekdebaby          Icon name: computer-laptop            Chassis: laptop         Machine ID: a95814c1cd184d5fbc55aa9ce988eab7            Boot ID: 374f993c3c60449685b0af9dbc264e53   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-53-generic       Architecture: x86-64  20:41:34 up 48 min,  1 user,  load average: 0.49, 0.29, 0.29

--- libkvdb/test/main.c
+++ libkvdb/test/main.c
@@ -17,13 +17,12 @@
 int main() 
 {	
   struct kvdb *db;
-	struct kvdb *db2;
   const char *key = "operating-systems";
   char *value;
   //kvdb_put(db, key, "three-easy-pieces");
   //value = kvdb_get(db, key); 
   kvdb_close(db);
   //printf("[%s]: [%s]\n", key, value);
-  free(value);
+  //free(value);
   return 0;
 }
