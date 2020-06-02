1caa4aae73d7f6e8cd34613df7e93fb005e5e6a3
tracer-nju <tracer@nju.edu.cn>
2020-06-02 21:55:15 +0800

    Static hostname: Derekdebaby          Icon name: computer-laptop            Chassis: laptop         Machine ID: a95814c1cd184d5fbc55aa9ce988eab7            Boot ID: ecd50da71c18472bbfe45afec7ffa01a   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-53-generic       Architecture: x86-64  21:55:15 up 49 min,  1 user,  load average: 0.07, 0.30, 0.35

--- libkvdb/test/main.c
+++ libkvdb/test/main.c
@@ -18,12 +18,20 @@ int main()
 {	
   struct kvdb *db;
   const char *key = "operating-systems";
+	const char *key2 = "hjh";
+	const char haha[4096 * 2];
+	for(int i = 0; i < 5000; i++)
+		haha[i] = 'H';
+	haha[5000] = '\0';
   char *value;  
 	panic_on(!(db = kvdb_open("a.db")), "cannot open db");
   kvdb_put(db, key, "three-easy-pieces");
+	kvdb_put(db, key2, haha);
   value = kvdb_get(db, key); 
-  kvdb_close(db);
   printf("[%s]: [%s]\n", key, value);
+	value = kvdb_get(db, key2);
+	printf("[%s]: [%s]\n", key2, value);
   free(value);
+  kvdb_close(db);
   return 0;
 }
