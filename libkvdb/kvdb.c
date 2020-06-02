c6b69d9797659d97a795ea7a66d2c03d8b734d27
tracer-nju <tracer@nju.edu.cn>
2020-06-02 21:30:41 +0800

    Static hostname: Derekdebaby          Icon name: computer-laptop            Chassis: laptop         Machine ID: a95814c1cd184d5fbc55aa9ce988eab7            Boot ID: ecd50da71c18472bbfe45afec7ffa01a   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-53-generic       Architecture: x86-64  21:30:41 up 25 min,  1 user,  load average: 0.70, 0.43, 0.45

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -84,7 +84,7 @@ void check_log(struct kvdb *db)
 {
 	log_t *log = malloc(sizeof(log_t));
 	read2(ADDREND, db->fd, &log->commit, PGSIZE);
-	printf("commit is %d, n is %d\n", log->commit, log->n);
+	//printf("commit is %d, n is %d\n", log->commit, log->n);
 	if(log->commit == 0)
 	{
 		printf("no need to check log\n");
@@ -169,7 +169,7 @@ int kvdb_put(struct kvdb *db, const char *key, const char *value)
 	}
 	else
 	{
-
+		printf("To be implemented!!\n");
 
 	}
 
@@ -177,7 +177,6 @@ int kvdb_put(struct kvdb *db, const char *key, const char *value)
 	write2(DATAEND, db->fd, &log->addr, PGSIZE * 2);
 	write2(ADDREND, db->fd, &log->commit, PGSIZE);
 	free(log);
-	check_log(db);
 	flock(db->fd, LOCK_UN);
   return 0;
 }
