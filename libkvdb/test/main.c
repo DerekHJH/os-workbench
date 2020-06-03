78b3b6154b567cd11abfaecb557318ba48fb7579
tracer-nju <tracer@nju.edu.cn>
2020-06-03 22:00:24 +0800

    Static hostname: Derekdebaby          Icon name: computer-laptop            Chassis: laptop         Machine ID: a95814c1cd184d5fbc55aa9ce988eab7            Boot ID: 53f0de39b0194554b6789176a8bac2de   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-53-generic       Architecture: x86-64  22:00:24 up 36 min,  1 user,  load average: 0.30, 0.27, 0.43

--- libkvdb/test/main.c
+++ libkvdb/test/main.c
@@ -19,6 +19,7 @@ char xixi[4100];
 int main() 
 {	
   struct kvdb *db;
+	struct kvdb *db2;
   const char *key = "operating-systems";
 	const char *key2 = "hjh";
 	for(int i = 0; i < 1024 * 1024 * 15; i++)
@@ -29,12 +30,13 @@ int main()
 	xixi[4096] = '\0';
   char *value;  
 	panic_on(!(db = kvdb_open("a.db")), "cannot open db");
+	panic_on(!(db2 = kvdb_open("a.db")), "cannot open db2");
   kvdb_put(db, key, "three-easy-pieces");
-	//kvdb_put(db, key2, haha);
+	kvdb_put(db2, key2, xixi);
   value = kvdb_get(db, key); 
-  //printf("[%s]: [%s]\n", key, value);
+  printf("[%s]: [%s]\n", key, value);
 	value = kvdb_get(db, key2);
-	//printf("[%s]: [%s]\n", key2, value);
+	printf("[%s]: [%s]\n", key2, value);
 	//kvdb_put(db, key2, "three-easy-pieces");
 	//value = kvdb_get(db, key2);
 	//printf("[%s]: [%s]\n", key2, value);
