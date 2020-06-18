925914b395f4460b54556575cd53aa750c87731f
181240020-Hu Junhao <1185276716@qq.com>
2020-06-18 22:52:33 +0800

    continue to read mkfs

--- kernel/include/vfs.h
+++ kernel/include/vfs.h
@@ -3,12 +3,22 @@
 
 #define BSIZE 512
 
+#define NDIRECT 12
+#define NINDIRECT (BSIZE / sizeof(uint))
+#define MAXFILE (NDIRECT + NINDIRECT)
+
+
 typedef struct _inode
 {
-
+	short type;
+	short nlink;
+	short major;
+	short minor;
+	unsigned int size;
+	unsigned int addrs[NDIRECT + 1];
 }__attribute__((packed))inode_t;
-#define IPB (BSIZE / sizeof(inode_t))
-#define BPB (BSIZE * 8)
+
+
 
 
 
@@ -17,16 +27,26 @@ typedef struct _inode
 #define FSSTART (1024 * 1024 / BSIZE)
 #define FSSIZE (1024 * 1024 * 64 / BSIZE)
 #define INODESTART FSSTART
-#define NINODEBLOCK (NINODES / IBP + 1)
+
+//middle
+#define IPB (BSIZE / sizeof(inode_t))
+#define BPB (BSIZE * 8)
+#define IBLOCK(i) ((i) / IPB + INODESTART)
+//midlle
+
+#define NINODEBLOCK (NINODES / IPB + 1)
 #define BMSTART (INODESTART + NINODEBLOCK) 
+
+#define BBLOCK(b) ((b) / BPB + BMSTART) //middle
+
 #define NBM (FSSIZE / BPB + 1) 
 #define DATASTART (BMSTART + NBM)
 #define NMETADATA (FSSTART + NINODEBLOCK + NBM)
 #define NDATA (FSSIZE - NMETADATA)
 
-#define NDIRECT 12
-#define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+
+
+
 
 
 
--- kernel/tools/mkfs.c
+++ kernel/tools/mkfs.c
@@ -7,6 +7,10 @@
 #include <sys/mman.h>
 #include <../include/vfs.h>
 
+#define uint unsigned int
+#define ushort unsigned short
+#define uchar unsigned char
+
 #define panic_on(cond, s)\
 	do\
 	{\
@@ -18,29 +22,70 @@
 	}while(0)
 
 #define IMG_SIZE (64 * 1024 * 1024)
-
-void write_block(int num, void *buf)
+int fd;
+uint8_t *disk;
+int freeinode = INODESTART;
+int freeblock = DATASTART;
+void write_block(uint bnum, void *buf)
 {
-  panic_on(lseek(fsfd, sec * BSIZE, 0) != sec * BSIZE, "\033[31m In write_block, lseek(fsfd, sec * BSIZE, 0) != sec * BSIZE\033[0m\n");
-	panic_on(write(fsfd, buf, BSIZE) != BSIZE, "\033[31m In write_block, write(fsfd, buf, BSIZE) != BSIZE\033[0m\n");
+  panic_on(lseek(fd, bnum * BSIZE, 0) != bnum * BSIZE, "\033[31m In write_block, lseek(fd, bnum * BSIZE, 0) != sec * BSIZE\033[0m\n");
+	panic_on(write(fd, buf, BSIZE) != BSIZE, "\033[31m In write_block, write(fd, buf, BSIZE) != BSIZE\033[0m\n");
+}
+void read_block(uint bnum, void *buf)
+{
+	panic_on(lseek(fd, bnum * BSIZE, 0) != bnum * BSIZE, "\033[31m In read_block, lseek(fd, bnum * BSIZE, 0) != sec * BSIZE\033[0m\n");
+	panic_on(read(fd, buf, BSIZE) != BSIZE, "\033[31m In read_block, read(fd, buf, BSIZE) != BSIZE\033[0m\n");
 }
 
+void write_inode(uint inum, inode_t *ip)
+{
+  char buf[BSIZE];
+  uint bn;
+  inode_t *tempip;
 
+  bn = IBLOCK(inum);
+  read_block(bn, buf);
+  tempip = ((inode_t *)buf) + (inum % IPB);
+  *tempip = *ip;
+  write_block(bn, buf);
+}
+void read_inode(uint inum, inode_t *ip)
+{
+  char buf[BSIZE];
+  uint bn;
+  inode_t *tempip;
 
+  bn = IBLOCK(inum);
+  read_block(bn, buf);
+  tempip = ((inode_t *)buf) + (inum % IPB);
+  *ip = *tempip;
+}
 
+uint ialloc(ushort type)
+{
+  uint inum = freeinode++;
+  inode_t inode;
 
-
-
-
-
-
+  bzero(&inode, sizeof(inode));
+  inode.type = type;
+  inode.nlink = 1;
+  inode.size = 0;
+  write_inode(inum, &inode);
+  return inum;
+}
 
 
 
 int main(int argc, char *argv[]) 
 {
-  int fd;
-  uint8_t *disk;
+	/*===========data structure check=========*/
+	panic_on(BSIZE % sizeof(inode_t) != 0, "\033[31m BSIZE mod sizeof(inode_t) != 0\033[0m\n");
+	panic_on(BSIZE % sizeof(dirent_t) != 0, "\033[31m BSIZE mod sizeof(dirent_t) != 0\033[0m\n");
+	printf("FSSTART is 0x%x\nFSSIZE is 0x%x\nINODESTART is 0x%x\nNINODEBLOCK is 0x%lx\nBMSTART is 0x%lx\nNBM is 0x%x\nDATASTART is 0x%lx\nNDATA is 0x%lx\n", FSSTART, FSSIZE, INODESTART, NINODEBLOCK, BMSTART, NBM, DATASTART, NDATA);
+
+
+  
+  
 	//char *dirname = argv[3];
   assert((fd = open(argv[2], O_RDWR)) > 0);
   assert((ftruncate(fd, IMG_SIZE)) == 0);
@@ -51,6 +96,7 @@ int main(int argc, char *argv[])
 	for(int i = FSSTART; i < FSSIZE; i++)
 		write_block(i, zeros);
 	
+
 
 
 
