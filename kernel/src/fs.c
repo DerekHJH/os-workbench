ad367e49540480ddcb86a2cbce4c906671f3b58a
tracer-nju <tracer@nju.edu.cn>
2020-06-25 20:34:52 +0800

    Static hostname: Derekdebaby          Icon name: computer-laptop            Chassis: laptop         Machine ID: a95814c1cd184d5fbc55aa9ce988eab7            Boot ID: a20feae3f24e4e88a2c4fe6f8073caf5   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-59-generic       Architecture: x86-64  20:34:52 up  9:43,  1 user,  load average: 0.61, 0.71, 0.43

--- kernel/src/fs.c
+++ kernel/src/fs.c
@@ -5,6 +5,10 @@ struct
   inode_t inode[NINODE];
 } icache;
 
+void log_write(buf_t *bp)
+{
+	bwrite(bp);
+}
 void iinit()
 {
   kmt->spin_init(&icache.lock, "icache");
@@ -14,17 +18,48 @@ void iinit()
   }
 }
 
-/*
+static inode_t *iget(device_t *dev, uint32_t inum)
+{
+  inode_t *ip, *empty;
+
+  kmt->spin_lock(&icache.lock);
+
+  // Is the inode already cached?
+  empty = 0;
+  for(ip = &icache.inode[0]; ip < &icache.inode[NINODE]; ip++)
+  {
+    if(ip->ref > 0 && ip->dev == dev && ip->inum == inum)
+    {
+      ip->ref++;
+      kmt->spin_unlock(&icache.lock);
+      return ip;
+    }
+    if(empty == 0 && ip->ref == 0)    // Remember empty slot.
+      empty = ip;
+  }
+
+  // Recycle an inode cache entry.
+  panic_on(empty == 0, "\033[31m iget: no inodes\n \033[0m");
+
+  ip = empty;
+  ip->dev = dev;
+  ip->inum = inum;
+  ip->ref = 1;
+  ip->valid = 0;
+  kmt->spin_unlock(&icache.lock);
+
+  return ip;
+}
+
 inode_t *ialloc(device_t *dev, short type)
 {
-  int inum;
-  struct buf *bp;
-  struct dinode *dip;
+  buf_t *bp;
+  dinode_t *dip;
 
-  for(inum = 1; inum < sb.ninodes; inum++)
+  for(int inum = 1; inum < NINODES; inum++)
   {
-    bp = bread(dev, IBLOCK(inum, sb));
-    dip = (struct dinode*)bp->data + inum%IPB;
+    bp = bread(dev, IBLOCK(inum));
+    dip = (dinode_t *)bp->data + inum % IPB;
     if(dip->type == 0)
     {  // a free inode
       memset(dip, 0, sizeof(*dip));
@@ -35,6 +70,29 @@ inode_t *ialloc(device_t *dev, short type)
     }
     brelse(bp);
   }
-  panic("ialloc: no inodes");
+  panic_on(1, "\033[31m ialloc: no inodes \033[0m\n");
 }
-*/
+
+void iupdate(inode_t *ip)
+{
+  buf_t *bp;
+  dinode_t *dip;
+
+  bp = bread(ip->dev, IBLOCK(ip->inum));
+  dip = (dinode_t *)bp->data + ip->inum % IPB;
+  dip->type = ip->type;
+  dip->major = ip->major;
+  dip->minor = ip->minor;
+  dip->nlink = ip->nlink;
+  dip->size = ip->size;
+  memmove(dip->addrs, ip->addrs, sizeof(ip->addrs));
+  log_write(bp);
+  brelse(bp);
+}
+inode_t *idup(inode_t *ip)
+{
+  kmt->spin_lock(&icache.lock);
+  ip->ref++;
+  kmt->spin_unlock(&icache.lock);
+  return ip;
+}
