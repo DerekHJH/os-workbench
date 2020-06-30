f121faf32eb245534f9f1b06a29b5a2120685982
tracer-nju <tracer@nju.edu.cn>
2020-06-30 11:46:52 +0800

    Static hostname: Derekdebaby          Icon name: computer-laptop            Chassis: laptop         Machine ID: a95814c1cd184d5fbc55aa9ce988eab7            Boot ID: 36368d2516914af081b91901282ff630   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-61-generic       Architecture: x86-64  11:46:52 up  2:01,  1 user,  load average: 0.21, 0.29, 0.27

--- kernel/include/stat.h
+++ kernel/include/stat.h
@@ -3,6 +3,15 @@
 #define T_DIR  1   // Directory
 #define T_FILE 2   // File
 
+#define SEEK_CUR  0
+#define SEEK_SET  1
+#define SEEK_END  2
+
+#define O_RDONLY  00000000
+#define O_WRONLY  00000001
+#define O_RDWR    00000002
+#define O_CREAT   00000100
+
 typedef struct stat 
 {
   short type;  // Type of file
--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -1,5 +1,5 @@
 #include <common.h>
-
+extern cpu_t cpuinfo[];
 file_t *ofile[NOFILE];
 static void vfs_init()
 {
@@ -8,6 +8,20 @@ static void vfs_init()
 	fileinit();
 	return;
 }
+static int fdalloc(file_t *f)
+{
+	int fd;
+	for(fd = 0; fd < NOFILE; fd++)
+	{
+    if(ofile[fd] == 0)
+		{
+      ofile[fd] = f;
+      return fd;
+    }
+  }
+  return -1;
+}
+
 static int vfs_write(int fd, void *buf, int count)
 {
 	return filewrite(ofile[fd], buf, count);
@@ -23,18 +37,109 @@ static int vfs_close(int fd)
 	fileclose(f);
 	return 0;
 }
+static inode_t *create(char *path, short type, short major, short minor)
+{
+  inode_t *ip, *dp;
+  char name[DIRSIZ];
+
+  if((dp = nameiparent(path, name)) == 0)return 0;
+  ilock(dp);
+
+  if((ip = dirlookup(dp, name, 0)) != 0)
+	{
+    iunlockput(dp);
+    ilock(ip);
+    if(type == T_FILE && ip->type == T_FILE)return ip;
+    iunlockput(ip);
+    return 0;
+  }
+
+  panic_on((ip = ialloc(dp->dev, type)) == 0, "\033[31m create: ialloc\n \033[0m");
+
+  ilock(ip);
+  ip->major = major;
+  ip->minor = minor;
+  ip->nlink = 1;
+  iupdate(ip);
+
+  if(type == T_DIR)
+	{  // Create . and .. entries.
+    dp->nlink++;  // for ".."
+    iupdate(dp);
+    // No ip->nlink++ for ".": avoid cyclic ref count.
+    panic_on(dirlink(ip, ".", ip->inum) < 0 || dirlink(ip, "..", dp->inum) < 0, "\033[31m create dots\n \033[0m");
+  }
+
+  panic_on(dirlink(dp, name, ip->inum) < 0, "\033[31m create: dirlink\n \033[0m");
+
+  iunlockput(dp);
+
+  return ip;
+}
 static int vfs_open(const char *pathname, int flags)
 {
-	return 0;
+  int fd;
+  file_t *f;
+  inode_t *ip;
+
+  if(flags & O_CREAT)
+	{
+    ip = create((char *)pathname, T_FILE, 0, 0);
+    if(ip == 0)return -1;
+  } 
+	else 
+	{
+    if((ip = namei((char *)pathname)) == 0)return -1;
+    ilock(ip);
+    if(ip->type == T_DIR && flags != O_RDONLY)
+		{
+      iunlockput(ip);
+      return -1;
+    }
+  }
+
+  if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0)
+	{
+    if(f)fileclose(f);
+    iunlockput(ip);
+    return -1;
+  }
+  iunlock(ip);
+
+  f->type = FD_INODE;
+  f->ip = ip;
+  f->off = 0;
+  f->readable = !(flags & O_WRONLY);
+  f->writable = (flags & O_WRONLY) || (flags & O_RDWR);
+  return fd;
 }
 static int vfs_lseek(int fd, int offset, int whence)
 {
-	return 0;
+	switch(whence)
+	{
+		case SEEK_SET:
+		{
+			ofile[fd]->off = offset;
+			break;
+		}
+		case SEEK_CUR:
+    {
+      ofile[fd]->off += offset;
+    	break;
+    }
+		case SEEK_END:
+    {
+      ofile[fd]->off = ofile[fd]->ip->size + offset;
+    	break;
+    }
+		default: return -1;
+	}
+	return ofile[fd]->off;
 }
 static int vfs_link(const char *oldpath, const char *newpath)
 {
 	char name[DIRSIZ];
-  struct inode *dp, *ip;
+  inode_t *dp, *ip;
 
 	if((ip = namei((char *)oldpath)) == 0)return -1;
 
@@ -70,9 +175,66 @@ bad:
   iunlockput(ip);
   return -1;
 }
+int namecmp(const char *s, const char *t)
+{
+  return strncmp(s, t, DIRSIZ);
+}
+static int isdirempty(inode_t *dp)
+{
+  int off;
+  dirent_t de;
+
+  for(off = 2 * sizeof(de); off < dp->size; off += sizeof(de))
+	{
+    panic_on(readi(dp, (char*)&de, off, sizeof(de)) != sizeof(de), "\033[31m isdirempty: readi\n \033[0m");
+    if(de.inode != 0)return 0;
+  }
+  return 1;
+}
 static int vfs_unlink(const char *pathname)
 {
-	return 0;
+	inode_t *ip, *dp;
+  dirent_t de;
+  char name[DIRSIZ];
+  uint32_t off;
+
+
+  if((dp = nameiparent((char *)pathname, name)) == 0)return -1;
+
+  ilock(dp);
+
+  // Cannot unlink "." or "..".
+  if(namecmp(name, ".") == 0 || namecmp(name, "..") == 0)goto bad;
+
+  if((ip = dirlookup(dp, name, &off)) == 0)goto bad;
+
+  ilock(ip);
+
+  panic_on(ip->nlink < 1, "\033[31m unlink: nlink < 1\n \033[0m");
+  if(ip->type == T_DIR && !isdirempty(ip))
+	{
+    iunlockput(ip);
+    goto bad;
+  }
+
+  memset(&de, 0, sizeof(de));
+  panic_on(writei(dp, (char*)&de, off, sizeof(de)) != sizeof(de), "\033[31m unlink: writei\n \033[0m");
+  if(ip->type == T_DIR)
+	{
+    dp->nlink--;
+    iupdate(dp);
+  }
+  iunlockput(dp);
+
+  ip->nlink--;
+  iupdate(ip);
+  iunlockput(ip);
+
+  return 0;
+
+bad:
+  iunlockput(dp);
+  return -1;
 }
 static int vfs_fstat(int fd, struct ufs_stat *buf)
 {
@@ -80,25 +242,30 @@ static int vfs_fstat(int fd, struct ufs_stat *buf)
 }
 static int vfs_mkdir(const char *pathname)
 {
+  inode_t *ip;
+  if((ip = create((char *)pathname, T_DIR, 0, 0)) == 0)return -1;
+  iunlockput(ip);
 	return 0;
 }
 static int vfs_chdir(const char *path)
 {
+  inode_t *ip;
+  task_t *curtask = cpuinfo[_cpu()].current;
+  
+  if((ip = namei((char *)path)) == 0)return -1;
+
+  ilock(ip);
+  if(ip->type != T_DIR)
+	{
+    iunlockput(ip);
+    return -1;
+  }
+  iunlock(ip);
+  iput(curtask->cwd);
+  curtask->cwd = ip;
 	return 0;
 }
-static int fdalloc(file_t *f)
-{
-	int fd;
-	for(fd = 0; fd < NOFILE; fd++)
-	{
-    if(ofile[fd] == 0)
-		{
-      ofile[fd] = f;
-      return fd;
-    }
-  }
-  return -1;
-}
+
 static int vfs_dup(int fd)
 {
 	int fd2;
