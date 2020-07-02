5f0e464e2111c2f4a520abc4634e4a5eed480465
tracer-nju <tracer@nju.edu.cn>
2020-07-02 16:16:43 +0800

    Static hostname: Derekdebaby          Icon name: computer-laptop            Chassis: laptop         Machine ID: a95814c1cd184d5fbc55aa9ce988eab7            Boot ID: f1c34c65be5d4c6dba66561128d2a59d   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-61-generic       Architecture: x86-64  16:16:43 up  7:30,  1 user,  load average: 0.20, 0.08, 0.15

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -111,6 +111,7 @@ static int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), 
 	int fd = 0;	
 	sprintf(dirname, "/proc/%d", task->id);
   sprintf(filename, "%s/name", dirname);
+	printf("askxvasjkas\n");
   vfs->mkdir(dirname);
   panic_on((fd = vfs->open(filename, O_CREAT | O_RDWR)) < 0, "\033[31m in vfs_init file open failed ! \n\033[0m");
   vfs->write(fd, (char *)name, strlen(name));
